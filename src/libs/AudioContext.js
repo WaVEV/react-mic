f58ff8679c65d8f7631fb1d6da3fbe1f76591874
emaLupi <emanuel.lupi91@gmail.com>
2019-08-06 20:26:34 -0300

    fix audio content

--- src/libs/AudioContext.js
+++ src/libs/AudioContext.js
@@ -1,13 +1,22 @@
-const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-let analyser = audioCtx.createAnalyser();
+let audioCtx;
+let analyser;
 
 const AudioContext  = {
 
   getAudioContext() {
+    if(audioCtx === undefined){
+      audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+    }
     return audioCtx;
   },
 
   getAnalyser() {
+    if(audioCtx === undefined){
+        audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+    }
+    if(analyser === undefined){
+        analyser = audioCtx.createAnalyser();
+    }
     return analyser;
   },
 
